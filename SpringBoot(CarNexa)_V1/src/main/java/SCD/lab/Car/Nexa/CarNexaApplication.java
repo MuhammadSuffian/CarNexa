c23707473f9f8b72b90ac4d285e85da6a6f8d99d
Yasir-Khan-7 <110191648+Yasir-Khan-7@users.noreply.github.com>
2023-10-31 11:02:26 -0700

    SpringBootstarter
    This Contain CarNexa Starter Files of Spring Boot Project along with 2 dependencies for web

---
+++ SpringBoot(CarNexa)_V1/src/main/java/SCD/lab/Car/Nexa/CarNexaApplication.java
@@ -0,0 +1,13 @@
+package SCD.lab.Car.Nexa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarNexaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarNexaApplication.class, args);
+	}
+
+}
---
+++ SpringBoot(CarNexa)_V1/src/test/java/SCD/lab/Car/Nexa/CarNexaApplicationTests.java
@@ -0,0 +1,13 @@
+package SCD.lab.Car.Nexa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarNexaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
